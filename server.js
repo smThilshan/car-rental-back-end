9bc6259b744ef399606c5f56d22d8bc1bbd16ca9
smThilshan <smthilshan@gmail.com>
2025-07-02 23:19:36 +0400

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
